41f9db1afcfa1ce58120ecc95fcc1deca869251e
JulianBenitez <julian.benitezg@outlook.com>
2023-06-14 21:34:21 -0500

    More remaps

--- config/nvim/lua/maps.lua
+++ config/nvim/lua/maps.lua
@@ -11,6 +11,10 @@ set_keymap('n', '<C-a>', 'gg<ESC>VG', { noremap = true, silent = true })
 set_keymap('n', 'ss', ':split<CR><C-w>j', { noremap = true, silent = true })
 set_keymap('n', 'sv', ':vsplit<CR><C-w>l', { noremap = true, silent = true })
 
+-- Move text
+set_keymap('v', 'J', [[:m '>+1<CR>gv=gv]], { noremap = true, silent = true })
+set_keymap('v', 'K', [[:m '<-2<CR>gv=gv]], { noremap = true, silent = true })
+
 -- Move between windows
 set_keymap('n', 's<left>', '<C-w>h', { noremap = true, silent = true })
 set_keymap('n', 's<up>', '<C-w>k', { noremap = true, silent = true })
@@ -33,4 +37,15 @@ set_keymap('n', '<leader>nh', ':nohl<CR>', { noremap = true, silent = true })
 
 -- Black hole register
 set_keymap('n', 'x', '"_x', { noremap = true, silent = true })
+set_keymap('x', '<leader>p', '"_dP', { noremap = true, silent = true }) -- Allow to paste over selection
+set_keymap('n', "<leader>d", '"_d', { noremap = true, silent = true })
+set_keymap('v', "<leader>d", '"_d', { noremap = true, silent = true })
+
+-- Copy to clipboard
+set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+set_keymap('n', '<leader>Y', '"+Y', { noremap = true, silent = true })
+
+-- Command
+set_keymap('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
