f574b078c975ec353636665d8746fa7a3646515d
JulianBenitez <julian.benitezg@outlook.com>
2023-10-30 18:08:38 -0500

    Migrate to LazyVim

--- config/nvim/after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
--- Find files using Telescope command-line sugar.
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<LEADER>ff', builtin.find_files, {})
-vim.keymap.set('n', '<LEADER>lg', builtin.live_grep, {})
-vim.keymap.set('n', '<LEADER>fb', builtin.buffers, {})
-vim.keymap.set('n', '<LEADER>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<LEADER>fg', builtin.git_files, {})
-
