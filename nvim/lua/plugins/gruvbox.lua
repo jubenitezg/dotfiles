e45fe3d93a2af2ebbc87d811b0cb00c103aed3d4
JulianBenitez <julian.benitezg@outlook.com>
2023-10-31 00:04:31 -0500

    Add Gruvbox `transparent_mode`

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,5 +1,7 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", version = false, opts = {
+    transparent_mode = true,
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
