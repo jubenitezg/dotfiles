5da0afea93f64222042093ceec1f24b83b742751
JulianBenitez <julian.benitezg@outlook.com>
2023-11-01 19:49:10 -0500

    Do not sync system clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set_keymap = vim.api.nvim_set_keymap
+
+set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
+set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
+set_keymap("n", "<leader>Y", '"+Y', { noremap = true, silent = true })
