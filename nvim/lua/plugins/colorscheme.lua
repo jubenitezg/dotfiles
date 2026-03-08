251916921a163110753ca1ab065a4ee343a6f6a6
JulianBenitez <julian.benitezg@outlook.com>
2026-03-08 00:00:35 -0500

    chore: catppuccin theme lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,17 @@
 return {
-  { "ellisonleao/gruvbox.nvim", version = false, opts = {
-    transparent_mode = true,
-  } },
   {
-    "AlexvZyl/nordic.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("nordic").load()
-    end,
+    opts = {
+      flavour = "mocha",
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordic",
+      colorscheme = "catppuccin",
     },
   },
 }
